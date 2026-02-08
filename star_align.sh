d12abba50a9a388d1bedf5381339e66031a26138
Shubham J <shubham.shubham04@gmail.com>
2026-02-08 16:15:01 +0100

    star_align.sh

--- star_align.sh
+++ star_align.sh
@@ -7,12 +7,13 @@
 #SBATCH--output=/lisc/data/scratch/course/2025w300106/joge/logs/STAR.%j.log
 #SBATCH--error=/lisc/data/scratch/course/2025w300106/joge/logs/STAR.%j.err
 #SBATCH--mail-type=END,FAIL
-#SBATCH--mail-user=shubham.joge@univie.ac.at
+#SBATCH--mail-user=shubham.shubham04@gmail.com
 
 ## ENVIRONMENT
 module load STAR
 module list
 
+## Execution
 STAR --genomeDir /lisc/data/scratch/course/2025w300106/joge/jaNemVect_STAR \
      --runThreadN 16 \
      --readFilesIn /lisc/data/scratch/course/2025w300106/joge/SRR35988120.fastq \
